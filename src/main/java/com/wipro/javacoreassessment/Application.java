73ee72d13de56daf824708a33809758116550c2f
ferreira456 <81253125+ferreira456@users.noreply.github.com>
2021-04-01 15:26:37 -0300

    Update Application.java

--- src/main/java/com/wipro/javacoreassessment/Application.java
+++ src/main/java/com/wipro/javacoreassessment/Application.java
@@ -1,7 +1,5 @@
 package com.wipro.javacoreassessment;
 
-import com.wipro.javacoreassessment.view.ApplicationView;
-
 public class Application {
     public static void main(String[] args) {
         
